112e32fc6bf55efab294af6b458b970df8246cc6
cdadams1888 <cadams@mynetsvs.com>
2017-10-07 10:22:13 -0400

    made changes to the nav bar, case studies, etc

---
+++ assets/js/caseStudies.js
@@ -0,0 +1,179 @@
+"use strict";
+
+// Case Study Box 1
+function additionalTextCaseStudy1() {
+    var box = document.getElementById('caseStudy1OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy1AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy1() {
+    var box = document.getElementById('caseStudy1OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy1AdditionalText').style.display = "none";
+}
+
+// Case Study Box 2
+function additionalTextCaseStudy2() {
+    var box = document.getElementById('caseStudy2OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy2AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy2() {
+    var box = document.getElementById('caseStudy2OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy2AdditionalText').style.display = "none";
+}
+
+// Case Study Box 3
+function additionalTextCaseStudy3() {
+    var box = document.getElementById('caseStudy3OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy3AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy3() {
+    var box = document.getElementById('caseStudy3OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy3AdditionalText').style.display = "none";
+}
+
+// Case Study Box 4
+function additionalTextCaseStudy4() {
+    var box = document.getElementById('caseStudy4OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy4AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy4() {
+    var box = document.getElementById('caseStudy4OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy4AdditionalText').style.display = "none";
+}
+
+// Case Study Box 5
+function additionalTextCaseStudy5() {
+    var box = document.getElementById('caseStudy5OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy5AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy5() {
+    var box = document.getElementById('caseStudy5OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy5AdditionalText').style.display = "none";
+}
+
+// Case Study Box 6
+function additionalTextCaseStudy6() {
+    var box = document.getElementById('caseStudy6OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy6AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy6() {
+    var box = document.getElementById('caseStudy6OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy6AdditionalText').style.display = "none";
+}
+
+// Case Study Box 7
+function additionalTextCaseStudy7() {
+    var box = document.getElementById('caseStudy7OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy7AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy7() {
+    var box = document.getElementById('caseStudy7OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy7AdditionalText').style.display = "none";
+}
+
+// Case Study Box 8
+function additionalTextCaseStudy8() {
+    var box = document.getElementById('caseStudy8OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy8AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy8() {
+    var box = document.getElementById('caseStudy8OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy8AdditionalText').style.display = "none";
+}
+
+// Case Study Box 9
+function additionalTextCaseStudy9() {
+    var box = document.getElementById('caseStudy9OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy9AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy9() {
+    var box = document.getElementById('caseStudy9OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy9AdditionalText').style.display = "none";
+}
+
+// Case Study Box 10
+function additionalTextCaseStudy10() {
+    var box = document.getElementById('caseStudy10OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy10AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy10() {
+    var box = document.getElementById('caseStudy10OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy10AdditionalText').style.display = "none";
+}
+
+// Case Study Box 11
+function additionalTextCaseStudy11() {
+    var box = document.getElementById('caseStudy11OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy11AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy11() {
+    var box = document.getElementById('caseStudy11OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy11AdditionalText').style.display = "none";
+}
+
+// Case Study Box 12
+function additionalTextCaseStudy12() {
+    var box = document.getElementById('caseStudy12OriginalText');
+    var originalTextHide = box.style.display = "none";
+    var alternativeTextShow = document.getElementById('caseStudy12AdditionalText').style.display = "block";
+}
+
+function originalTextCaseStudy12() {
+    var box = document.getElementById('caseStudy12OriginalText');
+    var originalTextShow = box.style.display = "block";
+    var alternativeTextHide = document.getElementById('caseStudy12AdditionalText').style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
